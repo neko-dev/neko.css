6181e96203e1f25e42c9af1dbd3d3d359936cffc
eyhn <cneyhn@gmail.com>
2017-04-20 10:20:26 +0800

    docs(gril): add gril examples

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,13 +1,13 @@
 import * as gulp from 'gulp'
 import * as sass from 'gulp-sass'
+import * as rename from 'gulp-rename'
 import * as autoprefixer from "autoprefixer"
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
 
 gulp.task('sass', () => {
   var plugins = [
-        autoprefixer(),
-        cssnano()
+        autoprefixer()
     ];
   return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -15,12 +15,24 @@ gulp.task('sass', () => {
     .pipe(gulp.dest('./dist/css'));
 })
 
-gulp.task('build', ['sass'], () => {
+gulp.task('cssnano', () => {
+  var plugins = [
+        cssnano()
+    ];
+  return gulp.src('./dist/css/**/*[!(.min)].css')
+    .pipe(postcss(plugins))
+    .pipe(rename(function (path) {
+      path.basename += ".min";
+    }))
+    .pipe(gulp.dest('./dist/css'));
+})
+
+gulp.task('build', ['sass','cssnano'], () => {
 
 })
 
 gulp.task('watch', () => {
-  gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('./scss/**/*.scss', ['sass','cssnano']);
 })
 
 gulp.task('dev',['build','watch'],()=>{
